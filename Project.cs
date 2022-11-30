db558896a7d6d8ce9f5a2e92128e295a62615553
MirFen <117736020+MirFen@users.noreply.github.com>
2022-11-30 16:09:33 +0100

    Initial commit

---
+++ Project.cs
@@ -0,0 +1,43 @@
+﻿namespace FirstProject
+{
+    public class Project
+    {
+        private static void Main(string[] args)
+        {
+            int i = 0;
+            int sum = 0;
+            int max = 0;
+
+            System.Console.WriteLine("To exit, type '0'");
+
+            do
+            {
+                string? value = System.Console.ReadLine();
+
+                if (int.TryParse(value, out i))
+                {
+                    // System.Console.WriteLine(i);
+                    sum += i;
+                    max = max > i ? max : i;
+
+                }
+                else
+                {
+                    System.Console.WriteLine("Incorrect value");
+                    i = -1;
+                }
+            } while (i != 0);
+
+
+            if (i == 0)
+            {
+                System.Console.WriteLine("Good bye " + "sum of numbers: " + sum + " max value: " + max);
+            }
+            else
+            {
+                System.Console.WriteLine("Good bye ");
+            }
+        }
+
+    }
+}
